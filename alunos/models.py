131f4c436c27839f9e54e143caa5aefb3028e77c
Joao Eurico <joao@charlesrobotics.com>
2023-02-12 13:51:28 -0300

    created Aluno entity - no migration yet

--- alunos/models.py
+++ alunos/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    nickname = models.CharField(max_length=100)
+    email = models.EmailField()
+    campo_de_estudo = models.CharField(max_length=100)
+    discordid = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+    data_nascimento = models.DateField()
+
+    def __str__(self):
+        return self.nome + ' - ' + self.nickname
