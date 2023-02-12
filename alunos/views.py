fba1e50f05d8ce3336de71aee7d48e44e65f3648
Joao Eurico <joao@charlesrobotics.com>
2023-02-12 15:20:19 -0300

    fixed css file import on static

--- alunos/views.py
+++ alunos/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Aluno
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'paginaalunos/index.html')
+    return render(request,  # request object
+                  'paginaalunos/index.html',  # the page to render
+                  {'alunos': Aluno.objects.all()},  # a context dictionary
+                  )
+
+# the method objects.all return dictionary with all attributes of the entity Aluno
